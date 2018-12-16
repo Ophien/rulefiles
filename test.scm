ba4b4651f3455e4e5d6a1963f5b4b1c57b0ead2c
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2018-12-16 16:42:37 -0200

    Update test.scm

--- test.scm
+++ test.scm
@@ -1,5 +1,2 @@
-urge: (please_user=1 novelty=1)
-goal: (please_user=1)
-
-#goal: (novelty=1 please_user=1)
-       u: (apple) I want an apple ^keep
+#goal: (novelty=0.24)
+       u: (eat apple) I want an apple ^keep
