30436223fbc3bba3adc713102f6d63bb9b0d6a2b
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2018-12-19 08:20:47 -0200

    Update sampleBot.scm

--- sampleBot.scm
+++ sampleBot.scm
@@ -1,6 +1,11 @@
 (define-public (findkiller)
   ; ... the process of finding the killer ...
   ; The answer should be a list of nodes wrapped in a ListLink
-  (List (Word "Bob") (Word "and") (Word "Alice")))
+  (List 
+      (Word "Bob") 
+      (Word "and") 
+      (Word "Alice") 
+  )
+)
 
 ?: ( who killed _* ) I think ^findkiller killed the '_0
