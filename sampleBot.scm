4fda854013f6bf9b8d799fd38b1de19371fe4ef9
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2018-12-19 09:10:49 -0200

    Update sampleBot.scm

--- sampleBot.scm
+++ sampleBot.scm
@@ -1,2 +1,4 @@
-#goal: (novelty=0.24)
-       u: ((who are you)) I am your GHOST!
+#goal: novelty=0.8
+#goal: please_user=0.4
+       s: ( what be you name ) I forgot; what's YOUR name, sweet wonderful human
+
