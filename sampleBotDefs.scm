3e4afa24abded1ad321cdcec12a79a0f691b844a
ophien <alysson.ribeiro.silva@gmail.com>
2018-12-21 14:31:05 -0200

    haha

--- sampleBotDefs.scm
+++ sampleBotDefs.scm
@@ -1,4 +1,4 @@
 (define-public (update_urge)
-               (psi-increase-urge response_user 0.1)
-               (psi-decrease-urge help_user 0.1)
+               (psi-increase-urge "response_user" 0.1)
+               (psi-decrease-urge "help_user" 0.1)
                (List (Word "")))
