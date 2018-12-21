6801a7f6c0e8e6d1ebfcd5689ea98552f627e7ee
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2018-12-21 13:18:47 -0200

    Update sampleBot.scm

--- sampleBot.scm
+++ sampleBot.scm
@@ -1,8 +1,6 @@
-(define-public (update_urge)((psi-decrease-urge help_user 0.1)(psi-increase-urge response_user 0.1)(List (Word ""))))
-
-(ghost-parse "#goal: (novelty=0.8) s: (what be you name) I forgot... what's YOUR name, sweet wonderful human ^keep")
-(ghost-parse "#goal: (novelty=0.8) u: (you have a cake) yes do you want some? 
+#goal: (novelty=0.8) s: (what be you name) I forgot... what's YOUR name, sweet wonderful human ^keep
+#goal: (novelty=0.8) u: (you have a cake) yes do you want some? 
                   a: (yes) here you go 
-                  a: (no) your loss")
-(ghost-parse "#goal: (help_user=1) u: (will you help me) Sure ! ^update_urge ^keep")
-(ghost-parse "#goal: (response_user=0.0) u: (will you help me) Nop, fuck off ! ^keep")
+                  a: (no) your loss
+#goal: (help_user=1) u: (will you help me) Sure ! ^update_urge ^keep
+#goal: (response_user=0.0) u: (will you help me) Nop, fuck off ! ^keep
