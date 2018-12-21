a9285f68bd3978106f900740e6e13d1197ece82e
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2018-12-21 10:50:48 -0200

    Update sampleBot.scm

--- sampleBot.scm
+++ sampleBot.scm
@@ -1,2 +1,4 @@
 #goal: (novelty=0.8) s: ( what be you name ) I forgot... what's YOUR name, sweet wonderful human ^keep
-#goal: (novelty=0.8) u: (you have a cake) yes do you want some? a: (yes) here you go a: (no) your loss
+#goal: (novelty=0.8) u: (you have a cake) yes do you want some? 
+                        a: (yes) here you go 
+                        a: (no) your loss
