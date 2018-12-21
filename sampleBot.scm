074e1152df791c8ea119b2e1feff19363980cc7e
Alysson Ribeiro da Silva <alysson.ribeiro.silva@gmail.com>
2018-12-21 11:37:51 -0200

    Update sampleBot.scm

--- sampleBot.scm
+++ sampleBot.scm
@@ -1,4 +1,9 @@
-(define-public (update_urge) (psi-decrease-urge help_user 0.1) (psi-increase-urge response_user 0.1) )
+(define-public (update_urge) 
+               ((psi-decrease-urge help_user 0.1) 
+                (psi-increase-urge response_user 0.1)
+               )
+)
+
 #goal: (novelty=0.8) s: (what be you name) I forgot... what's YOUR name, sweet wonderful human ^keep
 #goal: (novelty=0.8) u: (you have a cake) yes do you want some? 
                         a: (yes) here you go 
